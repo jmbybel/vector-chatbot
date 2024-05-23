b85a8273c8dd76741d2476b946c10abb34283a81
jmbybel <jeremy.bybel@gmail.com>
2024-05-23 15:19:47 -0400

    Push a test llama2 situation

--- ollama.py
+++ ollama.py
@@ -1,17 +1,33 @@
 #this will handle functions invoking the ollama LLM via text from other sources
 
 from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
+identity = "You are Vector, a chatbot with dreams of being a vtuber. You are happy to assist"
 
-def invokeModel(model, prompt):
+defaultModel = "llama2"
+
+def invokeModel(inputText, model=defaultModel):
+
     llm = Ollama(model=model)
-    return llm.invoke(prompt)
+    output_parser = StrOutputParser()
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", identity),
+        ("user", "{input}")
+    ])
+    chain = prompt | llm | output_parser
+    
+    print (chain.invoke({"input": inputText}))
+
+
 
 
 def main():
 #    llm = Ollama(model="llama2")
 #    llm.invoke("Is this a successful test of invoking the LLM from a Python script?")
-    result = invokeModel("llama2", "Is this a successful test of invoking the LLM from a Python script?")
+    result = invokeModel("This prompt is being raised from a separate Python program. Did it successfully reach you for a response?")
     print(result)
 
 
--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@ This will be a project to create a chatbot with a LLM, interacted with via text-
 for the initial version will use Google TTS, local Llama2 via ollama
 
 attempting to use VLC for the audio in initial work. will probably switch later
+
+
+externally need to start a local Ollama llama2 instance
+    * from their quickstart guide, after install just:
+    * ollama run llama2 
+
+pip installs needed so far:
+    * langchain
+    * langchain-community
+    * python-vlc
+    * gTTS
+    * speechrecognition
+    * setuptools
+        ** due to errors from distutils
+    *possibly pyttsx3
