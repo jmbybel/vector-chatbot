878d6790a121409e16a214561754692f2df1ce97
jmbybel <jeremy.bybel@gmail.com>
2024-05-23 14:30:06 -0400

    swap VLC playback in, test both TTS and STT

--- googleTTS.py
+++ googleTTS.py
@@ -1,32 +1,41 @@
 
 from gtts import gTTS
-from audioPlayer import play_mp3
+from audioPlayer import play_mp3_vlc, record_audio
 
 import os
+import time
 
 #hardcode audio file save/load location to be windows directory C:/programming/data/sound/gtts
+#TODO later move to a configurable spot
 baseDirectory = "C:/programming/data/sound/gtts"
 language = "en"
 
 #generate a hello world audio file with gtts
-#if directories do not exist, create them
 def generateHelloWorldAudio():
-    #if the file already exists, just return the file path
+    #if the file already exists, don't recreate hello world.
     if os.path.isfile(baseDirectory + "/helloWorld.mp3"):
         return baseDirectory + "/helloWorld.mp3"
 
-    tts = gTTS(text="Hello World", lang=language)
-    fullAudioFilePath = baseDirectory + "/helloWorld.mp3"
+    return generateAudioFile("Hello World", "helloWorld")
+
+#generate an audio file with gtts from a given line of text
+#return the full path of the audio file
+#optionally save the file with a given name, otherwise it saves as the current system time in millis
+def generateAudioFile(audioText, optionalFilename = None):
+    tts = gTTS(text=audioText, lang=language)
+    fullAudioFilePath = baseDirectory + "/" + str(int(time.time())) + ".mp3"
+    if optionalFilename is not None:
+        tts.save(baseDirectory + "/" + optionalFilename + ".mp3")
+        fullAudioFilePath = baseDirectory + "/" + optionalFilename + ".mp3"
     util_createPathIfMissing(baseDirectory)
     tts.save(fullAudioFilePath)
+
+    print("DEBUG", audioText)
+
     return fullAudioFilePath
 
-
 def playAudioFile(audioFile):
-    play_mp3(audioFile)
-#    os.system("start " + audioFile)
-
-
+    play_mp3_vlc(audioFile)
 
 #utility function to create directories if they do not exist
 def util_createPathIfMissing(path):
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 
 from langchain_community.llms import Ollama
+from audioPlayer import play_mp3_vlc, record_audio
+from googleTTS import generateAudioFile
 
 
-#This file will become a minimal launcher for this application. any sample/testing data will be removed.
+
+#This file will eventually become  launcher for this application. any sample/testing data will be removed.
 #other files will 
 # 1) handle interactions with the llama2 model specifically
 # 2) provide a general LLM wrapper to swap models trivially
-# 3) create a text-to-speech engine to convert model responses to speech, and play them back to the user
-# 4) create a speech-to-text engine to convert user speech to text, and pass it to the model
+# 3) DONE create a text-to-speech engine to convert model responses to speech, and play them back to the user
+# 4) DONE create a speech-to-text engine to convert user speech to text, and pass it to the model
+
+
+#record audio by calling record_audio, return the text from it, generate mp3 of the same text via gTTS
+#save the mp3 to the baseDirectory with a filename of the unix timestamp for now
+def testFeedbackLoop():
+    audioText = record_audio()
+    
+    resultAudio = generateAudioFile(audioText)
+    play_mp3_vlc(resultAudio)
+
 
 def main():
-    llm = Ollama(model="llama2")
-    llm.invoke("Is this a successful test of invoking the LLM from a Python script?")
+    testFeedbackLoop()
+#    llm = Ollama(model="llama2")
+#    llm.invoke("Is this a successful test of invoking the LLM from a Python script?")
 
 
 
